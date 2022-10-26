42068ece113480a6faa4d8d0dde3464302bb123d
Satria Syammahestatma <satriasyamm@gmail.com>
2022-10-26 19:25:16 +0700

    edit submit

--- frontend/src/pages/dashboard.js
+++ frontend/src/pages/dashboard.js
@@ -44,6 +44,47 @@ function Dashboard() {
       </table>
       <div className="justify-center py-96 align-middle">
         <div className="w-full bg-gray-500">
+      <select
+                  value={search}
+                  onChange={(e) => setSearch(e.target.value)}
+                >
+                  <option value="Aceh">Aceh</option>
+                  <option value="Sumatera Utara">Sumatera Utara</option>
+                  <option value="Sumatera Selatan">Sumatera Selatan</option>
+                  <option value="Sumatera Barat">Sumatera Selatan</option>
+                  <option value="Riau">Riau</option>
+                  <option value="Kepulauan Riau">Kepulauan Riau</option>
+                  <option value="Jambi">Jambi</option>
+                  <option value="Bengkulu">Bengkulu</option>
+                  <option value="Bangka Belitung">Bangka Belitung</option>
+                  <option value="Lampung">Lampung</option>
+                  <option value="Banten">Banten</option>
+                  <option value="DKI Jakarta">DKI Jakarta</option>
+                  <option value="Jawa Barat">Jawa Barat</option>
+                  <option value="Jawa Tengah">Jawa Tengah</option>
+                  <option value="DI Yogyakarta">DI Yogyakarta</option>
+                  <option value="Jawa Timur">Jawa Timur</option>
+                  <option value="Bali">Bali</option>
+                  <option value="NTB">NTB</option>
+                  <option value="NTT">NTT</option>
+                  <option value="Kalimantan Barat">Kalimantan Barat</option>
+                  <option value="Kalimantan Tengah">Kalimantan Tengah</option>
+                  <option value="Kalimantan Utara">Kalimantan Utara</option>
+                  <option value="Kalimantan Selatan">Kalimantan Selatan</option>
+                  <option value="Kalimantan Timur">Kalimantan Timur</option>
+                  <option value="Gorontalo">Gorontalo</option>
+                  <option value="Sulawesi Selatan">Sulawesi Selatan</option>
+                  <option value="Sulawesi Tenggara">Sulawesi Tenggara</option>
+                  <option value="Sulawesi Tengah">Sulawesi Tengah</option>
+                  <option value="Sulawesi Utara">Sulawesi Utara</option>
+                  <option value="Sulawesi Barat">Sulawesi Barat</option>
+                  <option value="Maluku">Maluku</option>
+                  <option value="Maluku Utara">Maluku Utara</option>
+                  <option value="Papua">Papua</option>
+                  <option value="Papua Barat">Papua Barat</option>
+                  
+
+        </select>
           <input
             onChange={(e) => {
               setSearch(e.target.value);
