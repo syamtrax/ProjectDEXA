d87c28df4ddecb4621a36e5c55e999f35b9ed725
hadidwirsty <m.hadid.2001@mail.ugm.ac.id>
2022-10-27 09:06:59 +0700

    Update tabel border

--- frontend/src/pages/dashboard.js
+++ frontend/src/pages/dashboard.js
@@ -98,7 +98,7 @@ function Dashboard() {
                 </thead>
                 <tbody>
                   {data.map((dat) => (
-                    <tr key={dat.id} className="py-8">
+                    <tr key={dat.id} className="py-8 border-2 border-gray-300">
                       <td className="w-1/5 text-center">{dat.Provinsi}</td>
                       <td className="w-1/5 text-center">{dat.namaObat}</td>
                       <td className="w-1/5 text-center">{dat.katObat}</td>
